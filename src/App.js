d679d042f0c930b4f782427d5476b4fbf2710057
tarupathak <113367480+tarupathak@users.noreply.github.com>
2023-01-17 22:14:33 +0530

    Used spread operator to shorten the code

--- src/App.js
+++ src/App.js
@@ -15,24 +15,8 @@ const App = () => {
     setContact((prevValue) => {
       if(name === "fName") {
         return{
-         fName : value,
-         lName : prevValue.lName,
-         email : prevValue.email
-        };
-      }
-      else if(name === "lName") {
-        return{
-        fName : prevValue.fName,
-        lName : value,
-        email : prevValue.email
-        };
-
-      }
-      else if(name === "email") {
-        return {
-        fName : prevValue.fName,
-        lName : prevValue.lName,
-        email : value
+         ...prevValue,
+         [name] : value;
         };
 
       }
